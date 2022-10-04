2134632df3b9eb12581bd8a39febca4b4eaf898c
Alhilal Anwar <anony7911@gmail.com>
2022-10-04 06:35:35 +0000

    dd

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/alternatif', [App\Http\Controllers\AlternatifController::class, 'index'])->name('alternatif');
-Route::get('/kriteria', [App\Http\Controllers\KriteriaController::class, 'index'])->name('kriteria');
-Route::get('/manajuser', [App\Http\Controllers\ManajuserController::class, 'index'])->name('manajuser');
-Route::get('/perhitungan', [App\Http\Controllers\PerhitunganController::class, 'index'])->name('perhitungan');
-Route::get('/rekomendasi', [App\Http\Controllers\RekomendasiController::class, 'index'])->name('rekomendasi');
 Auth::routes();
 //middleware
     Route::get('/alternatif', [App\Http\Controllers\AlternatifController::class, 'index'])->name('alternatif');
